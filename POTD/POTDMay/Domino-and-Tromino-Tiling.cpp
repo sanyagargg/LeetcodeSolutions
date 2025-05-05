202abf25f6cf3e773279f0ea9010f37e099f382d
Sanya Garg <133576817+sanyagargg@users.noreply.github.com>
2025-05-05 14:43:37 +0530

    [790] [Time Beats: 100.00%] [Memory Beats: 26.67%] - LeetPush

---
+++ POTD/POTDMay/Domino-and-Tromino-Tiling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numTilings(int n) {
+        const int MOD = 1e9 + 7;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 5;
+
+        // dp[i] = number of ways to tile a 2 x i board
+        vector<long long> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for (int i = 4; i <= n; ++i) {
+            dp[i] = (2 * dp[i - 1] % MOD + dp[i - 3]) % MOD;
+        }
+
+        return dp[n];
+    }
+};
+
+
+//Time : O(N)
+//Space: O(N)
