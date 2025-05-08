7b61f1afe9ca4437cd33367fa5b6b9936759981d
Sanya Garg <133576817+sanyagargg@users.noreply.github.com>
2025-05-09 01:36:01 +0530

    [81] [Time Beats: 100.00%] [Memory Beats: 55.60%] - LeetPush

---
+++ BinarySearch/Search-in-Rotated-Sorted-Array-II.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Check if we found the target
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            // If we can't decide which side is sorted, move left or right
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            // Left half is sorted
+            else if (nums[left] <= nums[mid]) {
+                // Check if target is in the sorted left half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                // Check if target is in the sorted right half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
