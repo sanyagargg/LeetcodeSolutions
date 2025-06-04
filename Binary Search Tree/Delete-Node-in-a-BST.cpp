caf94f417c7eabb1979a20755adde7394838bc5d
Sanya Garg <133576817+sanyagargg@users.noreply.github.com>
2025-06-04 23:06:49 +0530

    [450] [Time Beats: 100.00%] [Memory Beats: 63.00%] - LeetPush

---
+++ Binary Search Tree/Delete-Node-in-a-BST.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr)
+            return nullptr;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            // Found the node to delete
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                // Node with two children: Get inorder successor (min in right subtree)
+                TreeNode* minNode = getMin(root->right);
+                root->val = minNode->val;
+                root->right = deleteNode(root->right, minNode->val);
+            }
+        }
+
+        return root;
+    }
+
+private:
+    TreeNode* getMin(TreeNode* node) {
+        while (node && node->left)
+            node = node->left;
+        return node;
+    }
+};
